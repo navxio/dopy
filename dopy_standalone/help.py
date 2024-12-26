94d3213c8852c5218640c90c2ee1619fcc52b638
Navdeep Saini <navdeep@mailbox.org>
2024-12-26 20:03:39 +0530

    init help text

---
+++ dopy_standalone/help.py
@@ -0,0 +1,12 @@
+HELP_TEXT = """
+    dopy: Python without indentation
+
+    USAGE
+    dopy <my_module>.dopy
+    
+    FLAGS
+    ------------------------------------------
+    - h: Print this text
+    - t: Write the transpiled files
+    - T <target> : Write the transpiled files to a target output module
+"""
