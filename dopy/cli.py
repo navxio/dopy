3b0663d83771e548ab455f2c1c6456690c345609
Navdeep Saini <navdeep@mailbox.org>
2024-12-26 20:43:21 +0530

    init cli interface

---
+++ dopy/cli.py
@@ -0,0 +1,33 @@
+import argparse
+from .core import preprocess_do_end as preprocess
+from .help import HELP_TEXT
+
+"""
+cli interface
+"""
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Python without indentation")
+    # Create a mutually exclusive group that allows a single flag at a time
+    group = parser.add_mutually_exclusive_group()
+
+    group.add_argument(
+        "-t",
+        action="store_true",
+        help="Transpile modules in place, preserving dir structure",
+    )
+
+    group.add_argument(
+        "-T", nargs="?", help="Transpile module to optional target_name.py"
+    )
+
+    group.add_argument("-h", action="store_true", help="Show help text")
+    args = parser.parse_args()
+
+    if args.h:
+        print(HELP_TEXT)
+
+
+if __name__ == "__main__":
+    main()
